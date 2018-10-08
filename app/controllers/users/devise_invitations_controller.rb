bd4dbbf615df584eaf7540e3cfcf3424ce8d8153
Erwin Rodriguez <erwinrr@live.com>
2018-10-08 16:35:28 -0700

    fix bug for password setup page

--- app/controllers/users/devise_invitations_controller.rb
+++ app/controllers/users/devise_invitations_controller.rb
@@ -1,5 +1,6 @@
 class Users::DeviseInvitationsController < Devise::InvitationsController
   def edit
+    super
     @user = User.find_by_invitation_token(params[:invitation_token], true)
   end
 end
