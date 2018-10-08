6393fabd994cfa2333c3f10e4b69c2b3ff58d5b6
Erwin Rodriguez <erwinrr@live.com>
2018-10-08 15:57:27 -0700

    fixed password set view

--- app/controllers/users/devise_invitations_controller.rb
+++ app/controllers/users/devise_invitations_controller.rb
@@ -1,2 +1,5 @@
 class Users::DeviseInvitationsController < Devise::InvitationsController
+  def edit
+    @user = User.find_by_invitation_token(params[:invitation_token], true)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts 'SEED FILE'
 puts '*' * 200
 organization = Organization.create!(name: 'Kitfox Automotive')
 puts 'created organization'
-dealership1 = organization.groups.create!(name: 'Audi of Athens Dealership')
+dealership1 = organization.groups.create!(name: 'Cars of Athens Dealership')
 puts 'created department1'
 admin = dealership1.users.create!(email: 'admin@gmail.com', password: 'hola32', first_name: 'Erwin', last_name: 'Juarez')
 admin.add_role(:admin)
