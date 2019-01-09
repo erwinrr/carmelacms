8f81c095c37c5de0e5059216355adb7d029fa856
Erwin Rodriguez <erwinrr@live.com>
2019-01-08 17:36:12 -0800

    when device_token is set it registers  aws application endpoint arn with aws-ruby-sdk

--- app/controllers/api/v1/device_tokens_controller.rb
+++ app/controllers/api/v1/device_tokens_controller.rb
@@ -2,6 +2,16 @@ class Api::V1::DeviceTokensController < ApplicationController
     def set  
       @user = User.find(data_params[:user_id])
       @user.customer_profile.device_token = data_params[:device_token]
+      sns_client = Aws::SNS::Client.new({
+        region: 'us-west-1',
+        access_key_id: Rails.application.credentials.aws[:access_key_id],
+        secret_access_key: Rails.application.credentials.aws[:secret_access_key]    
+      })
+      resp = sns_client.create_platform_endpoint({
+        platform_application_arn: "arn:aws:sns:us-west-1:827783505259:app/APNS_SANDBOX/Apple-Dev-Push", # required
+        token: @user.customer_profile.device_token
+      })
+      @user.customer_profile.endpoint = resp[:endpoint_arn]
       respond_to do |format|
         if @user.customer_profile.save
           format.json {
